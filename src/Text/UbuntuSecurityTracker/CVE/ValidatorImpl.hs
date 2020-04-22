4c61191c77859eaad3db6dba31cc1c00ca19cf4d
pancho horrillo <pedrofelipe.horrillo@bbva.com>
2020-04-22 19:03:02 +0200

    feat: fill the priority field
    Co-authored-by: Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>

--- src/Text/UbuntuSecurityTracker/CVE/ValidatorImpl.hs
+++ src/Text/UbuntuSecurityTracker/CVE/ValidatorImpl.hs
@@ -13,6 +13,10 @@ fillStaged' :: Staged -> [Token] -> Staged
 fillStaged' s [] = s
 fillStaged' s ((Ignored _):ps) = fillStaged' s ps
 fillStaged' s ((Metadata k v):ps)
-  | k == "Candidate"   = fillStaged' s{name=Just v} ps
-  | k == "Description" = fillStaged' s{description=Just v} ps
-  | otherwise          = fillStaged' s ps
+  | k == "Candidate"                 = fill s{name=Just v}
+  | k == "Description"               = fill s{description=Just v}
+  | k == "Priority" && v == "high"   = fill s{priority=Just H}
+  | k == "Priority" && v == "medium" = fill s{priority=Just M}
+  | k == "Priority" && v == "low"    = fill s{priority=Just L}
+  | otherwise                        = fill s
+  where fill s' = fillStaged' s' ps
