e9260c7109b72ae081d2eec1fae7cef38eb822b3
pancho horrillo <pedrofelipe.horrillo@bbva.com>
2020-04-24 13:00:48 +0200

    feat: insert affected packages
    Co-authored-by: Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>

--- src/Text/UbuntuSecurityTracker/CVE/ValidatorImpl.hs
+++ src/Text/UbuntuSecurityTracker/CVE/ValidatorImpl.hs
@@ -8,20 +8,24 @@ module Text.UbuntuSecurityTracker.CVE.ValidatorImpl
 import Control.Monad
 import Data.UbuntuSecurityTracker.CVE
 import Data.UbuntuSecurityTracker.CVE.Token
+import qualified Data.UbuntuSecurityTracker.CVE.Package as P
 
 fillCVE :: [Token] -> Either String CVE
 fillCVE = foldM honorToken emptyCVE
 
 -- TODO: Fill isRemote field when a CVSS parser is available
 honorToken :: CVE -> Token -> Either String CVE
-honorToken s (Ignored _) = Right s
-honorToken s (Metadata k v)
-  | k == "Candidate" = Right s {name = Just v}
-  | k == "Description" = Right s {description = Just v}
+honorToken c (Ignored _) = Right c
+honorToken c (Metadata k v)
+  | k == "Candidate" = Right c {name = Just v}
+  | k == "Description" = Right c {description = Just v}
   | k == "Priority" =
-    if | v == "high" -> Right s {priority = Just H}
-       | v == "medium" -> Right s {priority = Just M}
-       | v == "low" -> Right s {priority = Just L}
+    if | v == "high" -> Right c {priority = Just H}
+       | v == "medium" -> Right c {priority = Just M}
+       | v == "low" -> Right c {priority = Just L}
        | otherwise -> Left "unknown priority value"
-  | otherwise = Right s
-honorToken s (RPS r p st Nothing) = Right s
+  | otherwise = Right c
+honorToken c (RPS r p s Nothing) = Right c
+honorToken c@CVE{affected=aps} (RPS r p s cv) = Right $ case P.mapStatus s cv of
+                                                  Nothing -> c
+                                                  Just st -> c{affected=(P.Package r p st:aps)}
