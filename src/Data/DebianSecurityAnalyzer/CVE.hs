9ded4e7ba928ecc0897574dec120ae7c85159e77
pancho horrillo <pedrofelipe.horrillo@bbva.com>
2020-04-27 15:21:46 +0200

    feat: getUnstableVersion returns the unstable_version from a list of packages
    Co-authored-by: Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>

--- src/Data/DebianSecurityAnalyzer/CVE.hs
+++ src/Data/DebianSecurityAnalyzer/CVE.hs
@@ -27,4 +27,4 @@ mapCVE U.CVE{ U.description=Nothing } = Left "CVE description missing"
 
 
 getUnstableVersion :: String -> [UP.Package] -> Maybe String
-getUnstableVersion = undefined
+getUnstableVersion p [] = Nothing
