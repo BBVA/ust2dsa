557e3d68c4fc21871e8c9b7aa00e0efc6f6b2451
pancho horrillo <pedrofelipe.horrillo@bbva.com>
2020-04-29 08:27:42 +0200

    feat: renderVulnerability
    Co-authored-by: Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>

--- src/Text/DebianSecurityAnalyzer/Database.hs
+++ src/Text/DebianSecurityAnalyzer/Database.hs
@@ -3,4 +3,9 @@ module Text.DebianSecurityAnalyzer.Database where
 import Data.DebianSecurityAnalyzer.CVE
 
 renderVulnerability :: CVE -> String
+renderVulnerability CVE { name=""
+                        , description=""
+                        , priority=Nothing
+                        , isRemote=Nothing
+                        , affected=[] } = ",,"
 renderVulnerability _ = "foo,,bar"
