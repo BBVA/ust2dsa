bdfa348c84b92736232ce36cf3dfcdf506cdf303
pancho horrillo <pancho.horrillo@bbva.com>
2020-05-13 16:40:01 +0200

    fix: add missing files from previous refactors
    Co-authored-by: Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>

---
+++ src/Text/DebianSecurityAnalyzer/DatabaseImpl.hs
@@ -0,0 +1,58 @@
+{-|
+Copyright 2020 Banco Bilbao Vizcaya Argentaria, S.A.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-}
+module Text.DebianSecurityAnalyzer.Database where
+
+import Data.DebianSecurityAnalyzer.CVE
+import qualified Data.UbuntuSecurityTracker.CVE.Package as UP
+import Data.Bool
+import Data.List
+import Data.Maybe
+
+renderVulnerability :: CVE -> String
+renderVulnerability CVE { name = n , description = d } =
+    let saneDescription = fmap replaceNewLines d
+    in n ++ ",," ++ saneDescription
+  where
+    replaceNewLines :: Char -> Char
+    replaceNewLines c = bool ' ' c ('\n' /= c)
+
+renderPackage :: String       -- Release
+              -> Int          -- Vulnerability Index (section offset)
+              -> String       -- Package Name
+              -> CVE          -- Vulnerability
+              -> Maybe String -- Formatted Output
+renderPackage _ _ _ CVE { affected = [] } = Nothing
+renderPackage r o p CVE { affected = aps
+                        , priority = pri
+                        , isRemote = rmt } =
+    Just $ intercalate "," [name, show o, flags, unstable_version, other_versions]
+  where
+    name = p
+    flags = [ 'S', getFlagUrgency pri, getFlagIsRemote rmt, getFlagIsFixAvailable r p aps ]
+    unstable_version = fromMaybe "" $ getUnstableVersion p aps
+    other_versions = unwords $ getOtherVersions p aps
+
+renderDebsecanDB :: String -> [CVE] -> String
+renderDebsecanDB r cs = "VERSION 1\n" ++ sections
+  where
+    sections = intercalate "\n\n" [ vulnerabilities, affected, sources ]
+    vulnerabilities = intercalate "\n" $ fmap renderVulnerability cs
+    affected = intercalate "\n" renderedAffected
+    renderedAffected = do
+      (i, c@CVE { affected = aps }) <- zip [0..] cs
+      p <- nub $ UP.name <$> aps
+      maybe [] return (renderPackage r i p c)
+    sources = ""
---
+++ src/Text/UbuntuSecurityTracker/CVEImpl.hs
@@ -0,0 +1,36 @@
+{-|
+Copyright 2020 Banco Bilbao Vizcaya Argentaria, S.A.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-}
+module Text.UbuntuSecurityTracker.CVEImpl
+  ( parseAndValidate
+  ) where
+
+import Data.Bifunctor
+import Text.Parsec
+
+import Control.Monad ((>=>))
+import Data.DebianSecurityAnalyzer.CVE (CVE (..), mapCVE)
+import Text.UbuntuSecurityTracker.CVE.Parser (parseWithErrors)
+import Data.UbuntuSecurityTracker.CVE.Token (Token)
+import Text.UbuntuSecurityTracker.CVE.Validator (fillCVE)
+
+parseAndValidate :: String -> String -> Either String CVE
+parseAndValidate filename content = first addFilenameToError $ parseAndValidate' content
+  where
+    addFilenameToError :: String -> String
+    addFilenameToError e = filename ++ ": " ++ e
+    
+    parseAndValidate' :: String -> Either String CVE
+    parseAndValidate' = parseWithErrors >=> fillCVE >=> mapCVE
