8995e3c85e45d9507c55b2fe1252f79318a0601f
pancho horrillo <pancho.horrillo@bbva.com>
2020-05-13 18:05:14 +0200

    style: fix spacing
    Co-authored-by: Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>

--- src/Data/DebianSecurityAnalyzer/CVE.hs
+++ src/Data/DebianSecurityAnalyzer/CVE.hs
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -}
 module Data.DebianSecurityAnalyzer.CVE
-  ( CVE (..)
+  ( CVE(..)
   , mapCVE
   , combineFilters
   , getUnstableVersion
@@ -25,7 +25,7 @@ module Data.DebianSecurityAnalyzer.CVE
   ) where
 
 import Data.DebianSecurityAnalyzer.CVEImpl
-  ( CVE (..)
+  ( CVE(..)
   , mapCVE
   , combineFilters
   , getUnstableVersion
--- src/Data/DebianSecurityAnalyzer/CVEImpl.hs
+++ src/Data/DebianSecurityAnalyzer/CVEImpl.hs
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -}
 module Data.DebianSecurityAnalyzer.CVEImpl
-  ( CVE (..)
+  ( CVE(..)
   , mapCVE
   , combineFilters
   , getUnstableVersion
