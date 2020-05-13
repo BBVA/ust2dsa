94bbb6c6b81dea683a059c2e61c880a1cab629d6
pancho horrillo <pancho.horrillo@bbva.com>
2020-05-13 18:08:39 +0200

    refactor: extract yet another CVEImpl from CVE
    Co-authored-by: Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>

--- src/Data/UbuntuSecurityTracker/CVEImpl.hs
+++ src/Data/UbuntuSecurityTracker/CVEImpl.hs
@@ -16,14 +16,36 @@ limitations under the License.
 
 {-# LANGUAGE DeriveGeneric #-}
 
-module Data.UbuntuSecurityTracker.CVE
+module Data.UbuntuSecurityTracker.CVEImpl
   ( CVE(..)
   , Priority(..)
   , emptyCVE
   ) where
 
-import Data.UbuntuSecurityTracker.CVEImpl
-  ( CVE(..)
-  , Priority(..)
-  , emptyCVE
-  )
+import qualified Data.UbuntuSecurityTracker.CVE.Package as P
+import GHC.Generics
+
+data Priority
+  = L
+  | M
+  | H
+  deriving (Generic, Show, Eq, Ord)
+
+data CVE =
+  CVE
+    { name :: Maybe String
+    , description :: Maybe String
+    , priority :: Maybe Priority
+    , isRemote :: Maybe Bool
+    , affected :: [P.Package]
+    }
+  deriving (Show, Eq)
+
+emptyCVE =
+  CVE
+    { name = Nothing
+    , description = Nothing
+    , priority = Nothing
+    , isRemote = Nothing
+    , affected = []
+    }
